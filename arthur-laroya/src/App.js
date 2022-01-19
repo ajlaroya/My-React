cff1baabddc448398c4c1635e9d071425cdfe1a9
ajlaroya <arthur.j.laroya@gmail.com>
2022-01-19 22:10:02 +1100

    initial design, adaptive, small animations, colour, typography

--- arthur-laroya/src/App.js
+++ arthur-laroya/src/App.js
@@ -1,45 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div class="h-screen flex grid overflow-hidden grid-cols-5 auto-rows-auto">
-    	<div class="box bg-emerald-500 text-white text-lg font-bold text-center p-10 row-start-1 row-span-full col-span-3">
+    <div class="">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5">
 
+        <div class="relative col-span-3 h-screen flex bg-emerald-700">
+          <div class="flex items-center justify-center m-auto space-x-4">
+            <div class="aspect-square w-48 h-48 bg-white rounded-full animate-bounce"></div>
+            <div class="aspect-square w-24 h-24 bg-slate-50 rounded-full animate-bounce"></div>
+            <div class="aspect-square w-12 h-12 bg-slate-100 rounded-full animate-bounce"></div>
+          </div>
+          <div class="absolute text-justify font-mono text-white bottom-0 left-0 p-10">
+            <p class="text-4xl font-extrabold">Arthur Laroya</p>
+            <p class="text-3xl">— design & code</p>
+            <p class="italic font-light">full-stack web dev focused on <br/>
+            modular design and functionality</p>
+          </div>
+        </div>
 
-      <div class="flex items-center justify-center m-auto space-x-4">
-         <div class="w-64 h-64 bg-white rounded-full"></div>
-         <div class="w-32 h-32 bg-slate-50 rounded-full"></div>
-         <div class="w-16 h-16 bg-slate-100 rounded-full"></div>
+        <div class="relative col-span-2 h-screen flex bg-emerald-100 text-emerald-600 font-mono text-3xl">
+          <div class="flex justify-center m-auto space-x-2">
+            <div class="aspect-square w-12 h-12 bg-emerald-700 rounded-full hover:animate-pulse"></div>
+            <div class="aspect-square w-24 h-24 bg-emerald-700 rounded-full hover:animate-pulse"></div>
+            <div class="aspect-square w-48 h-48 bg-emerald-700 rounded-full hover:animate-pulse"></div>
+          </div>
+          <div class="absolute left-0 top-0 p-10">
+            <a class="hover:underline underline-offset-1" href="#">A</a>
+          </div>
+          <div class="absolute bottom-0 left-0 p-10">
+            <a class="hover:underline underline-offset-1" href="#">P</a>
+          </div>
+          <div class="absolute bottom-0 right-0 p-10">
+            <a class="hover:underline underline-offset-1" href="#">C</a>
+          </div>
+        </div>
       </div>
-
-      <div className="absolute bottom-5 left-5 p-3">
-        <p class="
-        font-mono
-        		text-4xl
-        		font-bold
-        		text-white">
-        		Arthur Laroya
-        </p>
-        <p class="
-          font-mono
-            text-1xl
-            font-medium
-            p-2
-            text-white">
-            — Design & Code
-        </p>
-        <p class="
-            font-normal
-            text-white
-            font-mono">
-            Full-stack web developer focused on <br/>
-            modular design and functionality
-        </p>
-      </div>
-
-    </div>
-    	<div class="box bg-emerald-100 text-emerald-600 text-lg font-bold text-center p-10 row-start-1 row-span-full col-span-2">My Projects</div>
     </div>
   );
 }
