dfd7ea1b63222f5b38137d1a82d6745718861788
ajlaroya <arthur.j.laroya@gmail.com>
2022-02-20 13:00:27 +1100

    navbar component, assets

--- ajl-frontend/src/components/Navbar/Navbar.jsx
+++ ajl-frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
-
-import './Navbar.scss';
+import React, { useState } from "react";
+import { HiOutlineMenu, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+import "./Navbar.scss";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="ajl-logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "contact", "work", "skills"].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
 
-export default Navbar
+      <div className="app__navbar-menu">
+        <HiOutlineMenu onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+            {["home", "about", "contact", "work", "skills"].map((item) => (
+              <li key={item}>
+                <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+              </li>
+            ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
