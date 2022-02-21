3e16d7a61fecf689f92866156d403f2325227e0b
ajlaroya <arthur.j.laroya@gmail.com>
2022-02-21 22:36:22 +1100

    connecting sanity cms to react!

--- ajl-frontend/src/container/About/About.jsx
+++ ajl-frontend/src/container/About/About.jsx
@@ -3,15 +3,27 @@ import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import './About.scss';
+import { urlFor, client } from '../../client';
 
-const abouts = [
-  { title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01 },
-  { title: 'Web Design', description: 'I am a good web developer.', imgUrl: images.about02 },
-  { title: 'UI/UX', description: 'I am a good web developer.', imgUrl: images.about03 },
-  { title: 'Fullstack', description: 'I am a good web developer.', imgUrl: images.about04 },
-];
+// const abouts = [
+//   { title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01 },
+//   { title: 'Web Design', description: 'I am a good web developer.', imgUrl: images.about02 },
+//   { title: 'UI/UX', description: 'I am a good web developer.', imgUrl: images.about03 },
+//   { title: 'Fullstack', description: 'I am a good web developer.', imgUrl: images.about04 },
+// ];
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+
+    client.fetch(query)
+      .then((data) => {
+        setAbouts(data)
+      })
+  }, []);
+
   return (
     <>
       <h2 className="head-text">I Know That <span> Good Design </span><br /> means <span>Good Business</span>
@@ -26,7 +38,7 @@ const About = () => {
             className="app__profiles-item"
             key={ about.title + index }
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
