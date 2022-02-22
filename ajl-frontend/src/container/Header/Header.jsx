97dbe0531d859848ee332fbca4abca59b5622be5
ajlaroya <arthur.j.laroya@gmail.com>
2022-02-22 21:21:03 +1100

    higher order components, social media, nav dots

--- ajl-frontend/src/container/Header/Header.jsx
+++ ajl-frontend/src/container/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 
+import { AppWrap } from "../../wrapper";
 import { images } from "../../constants";
 import "./Header.scss";
 
@@ -17,7 +18,7 @@ const scaleVariants = {
 
 const Header = () => {
   return (
-    <div id="home" className="app__header app__flex">
+    <div className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 1.5 }}
@@ -60,8 +61,8 @@ const Header = () => {
         className="app__header-circles"
       >
         {[images.flutter, images.redux, images.sass].map((circle, index) => (
-          <div className="circle-cmp app__flex" key={`circle-index`}>
-            <img src={circle} alt="circle" />
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="profile_bg" />
           </div>
         ))}
       </motion.div>
@@ -69,4 +70,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default AppWrap(Header, "home");
