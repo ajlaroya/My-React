b2991fc0ad0fedeabd49e392d2cd02d5a07c49a3
ajlaroya <arthur.j.laroya@gmail.com>
2022-03-14 11:39:36 +1100

    react redux, apis, dark style, crypto complete

--- crypto_app/src/index.js
+++ crypto_app/src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
-
-import App from './App';
-import 'antd/dist/antd.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./app/store";
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,document.getElementById('root'));
+  <Router>
+    {/* Provider gives access to our App components the store variables */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById("root")
+);
---
+++ crypto_app/src/services/cryptoApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "3b6379ba5fmsh07efa41e9e364a4p116c1cjsnf40bb60a19cc",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
+
+// redux toolkit creates hooks to call all data instantly
+export const { useGetCryptosQuery } = cryptoApi;
