d433ca5edc1cce12ff26a56c0508e24eaa5dda66
ajlaroya <arthur.j.laroya@gmail.com>
2022-03-15 22:28:28 +1100

    bruh

---
+++ crypto_app/src/components/LineChart.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+import { Col, Row, Typography } from "antd";
+import {Chart as ChartJS} from 'chart.js/auto'
+
+const { Title } = Typography;
+
+const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
+
+  // pushes price history and timestamp to respective arrays   
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.push(coinHistory?.data?.history[i].price);
+    coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp*1000).toLocaleDateString());
+
+  }
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: 'Price In USD',
+        data: coinPrice,
+        fill: false,
+        backgroundColor: '#cb2b83',
+        borderColor: '#cb2b83',
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+
+  return (
+    <>
+      <Row className="chart-header">
+        <Title level={2} className="chart-title">
+          {coinName} Price Chart
+        </Title>
+        <Col className="price-container">
+          <Title level={5} className="price-change">
+            {coinHistory?.data?.change}%
+          </Title>
+          <Title level={5} className="current-price">
+            Current {coinName} Price: $ {currentPrice}
+          </Title>
+        </Col>
+      </Row>
+      <Line data={data} options={options} />
+    </>
+  );
+};
+
+export default LineChart;
--- crypto_app/src/services/cryptoApi.js
+++ crypto_app/src/services/cryptoApi.js
@@ -9,6 +9,7 @@ const baseUrl = "https://coinranking1.p.rapidapi.com";
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
+// redux reducer takes an action and the previous state of the application and returns the new state
 export const cryptoApi = createApi({
   reducerPath: "cryptoApi",
   baseQuery: fetchBaseQuery({ baseUrl }),
@@ -16,8 +17,14 @@ export const cryptoApi = createApi({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
     }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) => createRequest(`/coin/${coinId}/history?timePeriod=${timeperiod}`),
+    }),
   }),
 });
 
 // redux toolkit creates hooks to call all data instantly
-export const { useGetCryptosQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } = cryptoApi;
