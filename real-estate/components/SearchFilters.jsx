16e237f0d1865d0e62db0ab800d1362abcef2020
ajlaroya <arthur.j.laroya@gmail.com>
2022-03-22 21:26:11 +1100

    start of search filters

---
+++ real-estate/components/SearchFilters.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect} from 'react'
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import { MdCancel } from 'react-icons'
+import Image from 'next/image'
+
+const SearchFilters = () => {
+  return (
+    <>
+        searchFilters
+    </>
+  )
+}
+
+export default SearchFilters
