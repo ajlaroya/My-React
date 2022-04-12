f542e2c69d40d56060990dc252e2cd6ef4d2a321
ajlaroya <arthur.j.laroya@gmail.com>
2022-04-12 21:37:45 +1000

    featured image optimization

--- sari-sari/components/FeaturedPostCard.jsx
+++ sari-sari/components/FeaturedPostCard.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import moment from "moment";
 import Link from "next/link";
+import Image from "next/image";
 import { BsStars } from "react-icons/bs";
 
 const FeaturedPostCard = ({ post }) => (
   <Link href={`/post/${post.slug}`}>
     <div className="relative h-72 bg-white rounded-lg">
       <div className="bg-white absolute z-50 w-full h-full rounded-lg opacity-25 hover:opacity-0 ease-in-out duration-500 cursor-pointer hover:scale-125"></div>
-      <div
+      <div className="absolute rounded-lg w-full h-full">
+      <Image
+          src={post.featuredImage.url}
+          alt={post.title}
+          layout="fill"
+          quality="50"
+          className="object-cover rounded-lg"
+        />
+      </div>
+      {/* <div
         className="absolute rounded-lg bg-center bg-no-repeat bg-cover inline-block w-full h-72"
         style={{ backgroundImage: `url('${post.featuredImage.url}')` }}
-      />
+      /> */}
       <div className="flex flex-col rounded-lg p-4 items-center justify-center absolute w-full h-full">
         <p className="mb-4 font-semibold text-xs">
           {moment(post.createdAt).format("MMM DD, YYYY")}
         </p>
         <p className="sari-sari mb-4 font-bold text-center">{post.title}</p>
         <div className="flex absolute top-5 w-full">
-          <p className="inline ml-5 bg-black transparent p-1 rounded-full">
-            <BsStars className="text-white" />
+          <p className="inline ml-5 bg-black/80 p-2 rounded-full">
+            <BsStars className="text-white text-sm" />
           </p>
         </div>
       </div>
