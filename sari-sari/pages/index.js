04722544c02074bec1f85edfaa034fed425ae350
ajlaroya <arthur.j.laroya@gmail.com>
2022-04-14 21:06:40 +1000

    small fix

--- sari-sari/pages/index.js
+++ sari-sari/pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ posts }) {
         <title>sári-sarì</title>
 
         <meta charset="utf-8" />
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="title" content="Sari-Sari" />
         <meta
