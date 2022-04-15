3f36f968d1a2789b0df821a361618f37fba980e6
ajlaroya <arthur.j.laroya@gmail.com>
2022-04-15 14:16:08 +1000

    pwa fix

--- sari-sari/next.config.js
+++ sari-sari/next.config.js
@@ -2,18 +2,16 @@
 
 const withPWA = require("next-pwa");
 
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: true,
   images: {
     domains: ["media.graphassets.com"],
-  }
-}
-
-module.exports = nextConfig, withPWA({
+  },
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
   },
-})
+});
+
+module.exports = nextConfig;
