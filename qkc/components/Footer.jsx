ae56678f1000d46b5656eae4d264b3b365ae3909
ajlaroya <arthur.j.laroya@gmail.com>
2022-05-17 20:58:41 +1000

    success page, bug fixes, minor design fixes

--- qkc/components/Footer.jsx
+++ qkc/components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
+import { AiFillGithub, AiFillInstagram, AiOutlineTwitter, AiTwotoneStar } from 'react-icons/ai'
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 Quiet Kids Club. All rights reserved.</p>
+      <p>2022 Quiet Kids Club. All rights reserved. <br />Disclaimer: this is a demo store with fake products</p>
       <p className="icons">
-        <AiFillInstagram />
-        <AiOutlineTwitter />
+        <a href="https://www.arthurlaroya.com/" rel="icon" target="_blank"><AiTwotoneStar /></a>
+        <a href="https://github.com/ajlaroya" rel="icon" target="_blank"><AiFillGithub /></a>
       </p>
     </div>
   )
--- qkc/pages/product/[slug].js
+++ qkc/pages/product/[slug].js
@@ -12,7 +12,12 @@ import { useStateContext } from "../../context/StateContext";
 const ProductDetails = ({ products, product }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const { decQty, incQty, qty, onAdd } = useStateContext();
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+    setShowCart(true);
+  }
 
   return (
     <div>
@@ -59,7 +64,7 @@ const ProductDetails = ({ products, product }) => {
               <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
-              <span className="num" onClick="">
+              <span className="num">
                 {qty}
               </span>
               <span className="plus" onClick={incQty}>
@@ -75,7 +80,7 @@ const ProductDetails = ({ products, product }) => {
             >
               Add to Cart
             </button>
-            <button type="button" className="buy-now" onClick="">
+          <button type="button" className="buy-now" onClick={handleBuyNow}>
               Buy Now
             </button>
           </div>
