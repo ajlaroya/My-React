93b80be46ff875080555346b9ba2d7d89770c710
ajlaroya <arthur.j.laroya@gmail.com>
2022-05-22 15:00:56 +1000

    ajl3 init, next, tailwind, TS, pages/components

---
+++ ajl3/components/aboutus.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Member from "./member";
+
+const AboutUs: React.FC = () => {
+  return (
+    <section className={`flex flex-col bg-white py-20 text-3xl md:text-4xl`}>
+      <div className="container mx-auto px-11">
+        <p className="leading-tight max-w-5xl mx-auto text-4xl tracking-tight">
+          <strong>I will help you ship better apps, faster, smarter. </strong>
+          As an expert full stack web developer I have created the best user
+          experiences in some of the most popular apps worldwide.
+        </p>
+      </div>
+      <div className='container mx-auto px-11 text-center mt-28'>
+        <h2>Homemade Web Dev</h2>
+        <div className='mt-2'>from Melbourne, AU</div>
+        <div className='mt-5 grid grid-cols-1'>
+            <Member id='arthur' name='Arthur' socialId='@ajlaroya' link="https://github.com/ajlaroya" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
---
+++ ajl3/components/member.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+  id: string;
+  name: string;
+  socialId: string;
+  link: string;
+}
+
+const Member: React.FC<Props> = ({ id, name, socialId, link }) => {
+  return (
+    <div>
+      <Image
+        src={`/assets/ajl_face_${id}.svg`}
+        alt={name}
+        width={400}
+        height={400}
+      />
+      <div className="text-2xl xl:text-3xl">{name}</div>
+      <div className="text-xl">
+          <Link href={link}>
+              <a href="" target="_blank">{socialId}</a>
+          </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Member;
