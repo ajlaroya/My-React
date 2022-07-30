5014587320d8a7321b5c46e8fc5bd5b5c90f5142
ajlaroya <arthur.j.laroya@gmail.com>
2022-07-30 12:30:04 +1000

    final cha cha changes

--- plume/client/src/api/index.js
+++ plume/client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+const API = axios.create({ baseURL: 'https://plume-social.herokuapp.com/' })
 
 // sends token to each API request
 API.interceptors.request.use((req) => {
--- plume/server/index.js
+++ plume/server/index.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
     res.send("Plume is running!")
 })
 
-const PORT = process.env.MONGODB_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGODB_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT} `)))
