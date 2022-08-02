6da8110f31f770a583859bb6acf54ec06f7ec6e5
ajlaroya <arthur.j.laroya@gmail.com>
2022-08-02 20:45:25 +1000

    videocard component

--- azalea/components/Navbar.tsx
+++ azalea/components/Navbar.tsx
@@ -11,9 +11,9 @@ import Logo from '../utils/shape-57.svg'
 
 const Navbar = () => {
   return (
-    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-100 py-2 px-4">
         <Link href="/">
-            <div className="w-[50px] md:w-[75px]">
+            <div className="w-[40px] md:w-[50px]">
                 <Image 
                     className="cursor-pointer"
                     src={Logo}
