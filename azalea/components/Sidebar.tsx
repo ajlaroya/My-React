bb97eab1fb3c1e67d5374cc90595b6cb09e96e5f
ajlaroya <arthur.j.laroya@gmail.com>
2022-08-09 21:18:04 +1000

    minor changes n fixes

--- azalea/components/Sidebar.tsx
+++ azalea/components/Sidebar.tsx
@@ -9,14 +9,16 @@ import { ImCancelCircle } from 'react-icons/im'
 import Discover from './Discover'
 import SuggestedAccounts from './SuggestedAccounts'
 import Footer from './Footer'
+import useAuthStore from '../store/authStore'
 
 
-const Sidebar = () => {
-  const [showSidebar, setShowSidebar] = useState(true)
+const Sidebar: NextPage = () => {
+  const [showSidebar, setShowSidebar] = useState<Boolean>(true)
+  const { pathname } = useRouter();
+  const { fetchAllUsers, allUsers }: any = useAuthStore();
 
-  const userProfile = false;
-
-  const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-purple-400 rounded'
+  const activeLink = 'flex items-center gap-2 hover:bg-primary px-3 py-2 justify-center xl:justify-start cursor-pointer font-semibold text-purple-400 rounded';
+  const normalLink = 'flex items-center gap-2 hover:bg-primary px-3 py-2 justify-center xl:justify-start cursor-pointer font-semibold rounded';
 
   return (
     <div>
@@ -30,15 +32,18 @@ const Sidebar = () => {
         <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
           <div className="xl:border-b-2 border-gray-100 xl:pb-4">
             <Link href="/">
-              <div className={normalLink}>
-                <p className="text-2xl"><AiFillHome /></p>
+              <div className={pathname === '/' ? activeLink : normalLink}>
+                <span className="text-xl"><AiFillHome /></span>
                 <span className="text-xl hidden xl:block">For You</span>
               </div>
             </Link>
           </div>
 
           <Discover />
-          <SuggestedAccounts />
+          <SuggestedAccounts 
+            fetchAllUsers={fetchAllUsers}
+            allUsers={allUsers}
+          />
           <Footer />
         </div>
       )}
--- azalea/utils/constants.tsx
+++ azalea/utils/constants.tsx
@@ -1,27 +1,30 @@
-import { BsCode, BsEmojiSunglasses } from 'react-icons/bs';
+import { BsCode, BsEmojiSunglasses, BsFillEmojiLaughingFill } from 'react-icons/bs';
 import { GiCakeSlice, GiGalaxy, GiLipstick } from 'react-icons/gi';
 import { FaPaw, FaMedal, FaGamepad } from 'react-icons/fa';
+import { AiFillCode } from 'react-icons/ai';
+import { RiFootprintFill, RiGamepadFill } from 'react-icons/ri'
+import { MdFastfood } from 'react-icons/md';
 
 export const topics = [
   {
     name: 'coding',
-    icon: <BsCode />,
+    icon: <AiFillCode />,
   },
   {
     name: 'comedy',
-    icon: <BsEmojiSunglasses />,
+    icon: <BsFillEmojiLaughingFill />,
   },
   {
     name: 'gaming',
-    icon: <FaGamepad />,
+    icon: <RiGamepadFill />,
   },
   {
     name: 'food',
-    icon: <GiCakeSlice />,
+    icon: <MdFastfood />,
   },
   {
     name: 'dance',
-    icon: <GiGalaxy />,
+    icon: <RiFootprintFill />,
   },
   {
     name: 'beauty',
