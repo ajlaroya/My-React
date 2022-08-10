b510529f7eb78596c4e59abd9972c8840a5c6310
ajlaroya <arthur.j.laroya@gmail.com>
2022-08-10 21:28:09 +1000

    daisy ui theme fix

--- azalea/tailwind.config.js
+++ azalea/tailwind.config.js
@@ -42,12 +42,11 @@ module.exports = {
   plugins: [require("daisyui")],
   daisyui: {
     styled: true,
-    themes: true,
+    themes: false,
     base: true,
     utils: true,
     logs: true,
     rtl: false,
     prefix: "",
-    darkTheme: "light",
   },
 };
