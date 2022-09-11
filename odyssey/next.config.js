245fe78204d0321b9d0b5ebb81a4745dc25ba44e
ajlaroya <arthur.j.laroya@gmail.com>
2022-09-11 12:32:44 +1000

    product detail pages, collections

--- odyssey/next.config.js
+++ odyssey/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['external-content.duckduckgo.com'],
+    domains: ['external-content.duckduckgo.com','cdn.shopify.com'],
   },
 }
 
