af558bdceaf69610d81dad0b60de429541094af9
ajlaroya <arthur.j.laroya@gmail.com>
2022-10-01 15:33:36 +1000

    faq'n deets omitted

--- arvoblooms/pages/faq.jsx
+++ arvoblooms/pages/faq.jsx
@@ -9,23 +9,23 @@ const faqs = [
   {
     question: "How far in advance do I need to order for delivery?",
     answer:
-      "Flowers Vasette offer same-day delivery for any orders placed before 12pm. Alternatively, if you would like your flowers to be delivered before 12pm, the order must be placed by close of business the day prior, at the latest. We are also happy to take any orders as far in advance as you wish. Simply order your flowers online or through contacting our team on +61 3 9419 4988 and we will be sure to organise beautiful blooms to be delivered anywhere across Melbourne.",
+      "Flowery Studios offer same-day delivery for any orders placed before 12pm. Alternatively, if you would like your flowers to be delivered before 12pm, the order must be placed by close of business the day prior, at the latest. We are also happy to take any orders as far in advance as you wish. Simply order your flowers online or through contacting our team on +61 X XXXX XXXX and we will be sure to organise beautiful blooms to be delivered anywhere across Melbourne.",
   },
   {
     question:
       "Are there times during the year that flowers are more expensive?",
     answer:
-      "Working with a natural, seasonal product does see flower prices vary throughout the year. Whilst flower prices are ultimately a sum of supply and demand, Flowers Vasette remain committed to maintaining the highest quality florals at a very reasonable price. Our team are always available for contact via telephone at +61 3 9419 4988 to discuss floral needs and prices with you.",
+      "Working with a natural, seasonal product does see flower prices vary throughout the year. Whilst flower prices are ultimately a sum of supply and demand, Flowery Studios remain committed to maintaining the highest quality florals at a very reasonable price. Our team are always available for contact via telephone at +61 X XXXX XXXX to discuss floral needs and prices with you.",
   },
   {
     question: "Do you have Wedding/Celebration packages?",
     answer:
-      "We have specialist in-house event stylists who offer years of experience in wedding and event florals. Flowers Vasette offer bespoke floral arrangements and installations for all of our clients, whatever the size of your event. Through an initial consultation with one of our specialist stylists, Flowers Vasette will then work with you to organise the highest-quality florals for your wedding or celebration party. Contact our team on +61 3 9419 4988 to begin your planning process.",
+      "We have specialist in-house event stylists who offer years of experience in wedding and event florals. Flowery Studios offer bespoke floral arrangements and installations for all of our clients, whatever the size of your event. Through an initial consultation with one of our specialist stylists, Flowery Studios will then work with you to organise the highest-quality florals for your wedding or celebration party. Contact our team on +61 X XXXX XXXX to begin your planning process.",
   },
   {
     question: "What if the flowers I want are not in season?",
     answer:
-      "Flowers Vasette offer a vast range of flowers, both local and imported. Our flowers can be arranged for same day delivery, whilst also being planned well in advance. Due to the nature of flower markets and the weather conditions, the seasonal varieties that we work with cannot always be certain. If you have a specific floral request, your safest option is to contact our team on +61 3 9419 4988 and they can consult with you accordingly. The beauty of Mother Nature is her expansive offerings, meaning there are always incredible floral alternatives on offer (that sometimes come when we least expect it).",
+      "Flowery Studios offer a vast range of flowers, both local and imported. Our flowers can be arranged for same day delivery, whilst also being planned well in advance. Due to the nature of flower markets and the weather conditions, the seasonal varieties that we work with cannot always be certain. If you have a specific floral request, your safest option is to contact our team on +61 X XXXX XXXX and they can consult with you accordingly. The beauty of Mother Nature is her expansive offerings, meaning there are always incredible floral alternatives on offer (that sometimes come when we least expect it).",
   },
   // More questions...
 ];
