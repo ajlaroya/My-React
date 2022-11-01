680855d100478496593cafc9dceffe475ebd0300
ajlaroya <arthur.j.laroya@gmail.com>
2022-11-01 19:44:45 +1100

    loader fix

--- flowery/app/flowers/[productHandle]/loading.jsx
+++ flowery/app/flowers/[productHandle]/loading.jsx
@@ -1,7 +1,7 @@
-import Loader from '../../../components/Loader'
-
 export default function Loading() {
   return (
-    <Loader />
+    <div className="flex justify-center items-center min-h-screen bg-zinc-100">
+      <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32" />
+    </div>
   );
 }
